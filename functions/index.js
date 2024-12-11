32d007c2068d9b1b5da4846ee037c57900f59e5a
adimpactcode <firat.lskp@gmail.com>
2024-12-11 11:24:13 +0100

    Resolved merge conflicts in flutterflow

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.onUserDeleted = functions.auth.user().onDelete(async (user) => {
-  let firestore = admin.firestore();
-  let userRef = firestore.doc("users/" + user.uid);
-  await firestore.collection("users").doc(user.uid).delete();
+  const firestore = admin.firestore();
+  const userRef = firestore.doc(`users/${user.uid}`);
+  await userRef.delete();
 });
